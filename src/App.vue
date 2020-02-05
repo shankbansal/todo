d39752fd630debbbf880be8ca447e0a3deddac30
shankbansal <50798143+shankbansal@users.noreply.github.com>
2020-02-05 16:59:46 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="md-layout md-gutter md-alignment-center">
+    <div class="md-layout-item md-large-size-60 md-medium-size-60 md-small-size-80 md-xsmall-size-80">
+      <h1>To-do List</h1>
+      <md-field>
+        <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add to-do"></md-input>
+      </md-field>
+
+      <h3>To-do Items</h3>
+      <ul class="todos">
+        <li v-for="todo in todos" :key="todo.id">
+          <div style="display:flex;align-items:center">
+          <span>{{ todo.label }}</span>
+          <md-button class="md-raised md-primary md-dense" @click="markCompleted(todo)">Mark Completed</md-button>
+          <md-button  class="md-raised md-accent md-dense" @click="removeTodo(todo)">Remove</md-button>
+          </div>
+        </li>
+      </ul>
+      <h3>Completed Items</h3>
+      <ul class="todos">
+        <li v-for="todo in completedTodo" :key="todo.id">
+          <div style="display:flex;align-items:center">
+          <span>{{ todo.label }}</span>
+          <md-button class="md-raised md-accent md-dense" @click="removeFromCompleted(todo)">Remove</md-button>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      completedTodo: [],
+      currentTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    markCompleted(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+      this.completedTodo.push(todo);
+    },
+    removeFromCompleted(todo) {
+      var index = this.completedTodo.indexOf(todo);
+      this.completedTodo.splice(index, 1);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import VueMaterial from 'vue-material';
+import "vue-material/dist/vue-material.min.css";
+import 'vue-material/dist/theme/default-dark.css';
+
+Vue.use(VueMaterial);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: x => {
+    return x(App);
+  }
+}).$mount("#app");
